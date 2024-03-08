81a0434f15856627ba7fd15f3750821c316cdff7
Tran Quang Huy <107989088+tranquanghuy-09@users.noreply.github.com>
2024-03-08 16:20:15 +0700

    complete config API-Gateway

---
+++ API_Gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.example.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
